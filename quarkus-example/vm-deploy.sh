08d9833284b2dd705c78097e05ec917e63f3d7ff
Staffan Olsson <staffan@repos.se>
2019-09-21 22:52:48 +0200

    UberJar is really impractical for docker builds

--- quarkus-example/vm-deploy.sh
+++ quarkus-example/vm-deploy.sh
@@ -2,7 +2,7 @@
 [ -z "$DEBUG" ] || set -x
 set -e
 
-(cd app && mvn clean package)
+(cd app && mvn -Pjar clean package)
 multipass transfer app/target/ystack-demo-app-runner.jar demo-ystack-app:/home/multipass/ystack-demo-app-runner.jar
 #multipass exec demo-ystack-app -- sudo apt-get install -y --no-install-recommends openjdk-11-jre-headless
 multipass exec demo-ystack-app -- java -jar /home/multipass/ystack-demo-app-runner.jar
