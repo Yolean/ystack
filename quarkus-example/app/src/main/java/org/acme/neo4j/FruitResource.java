40ed6d5a768994313f8ff57b7cfe613643adc0bb
Staffan Olsson <staffan@repos.se>
2019-09-21 21:46:50 +0200

    quarkus.io/guides/neo4j-guide#creating-nodes

--- quarkus-example/app/src/main/java/org/acme/neo4j/FruitResource.java
+++ quarkus-example/app/src/main/java/org/acme/neo4j/FruitResource.java
@@ -1,18 +1,25 @@
 package org.acme.neo4j;
 
+import java.net.URI;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 
 import org.neo4j.driver.Driver;
+import org.neo4j.driver.Values;
 import org.neo4j.driver.async.AsyncSession;
+import org.neo4j.driver.exceptions.NoSuchRecordException;
 
 @Path("fruits")
 @Produces(MediaType.APPLICATION_JSON)
@@ -35,6 +42,49 @@ public class FruitResource {
       )
       .thenApply(Response::ok)
       .thenApply(ResponseBuilder::build);
+  }
+
+  @GET
+  @Path("{id}")
+  public CompletionStage<Response> getSingle(@PathParam("id") Long id) {
+    AsyncSession session = driver.asyncSession();
+    return session
+      .readTransactionAsync(tx ->
+        tx.runAsync("MATCH (f:Fruit) WHERE id(f) = $id RETURN f", Values.parameters("id", id))
+      )
+      .thenCompose(fn -> fn.singleAsync())
+      .handle((record, exception) -> {
+        if(exception != null) {
+          Throwable source = exception;
+          if(exception instanceof CompletionException) {
+            source = ((CompletionException)exception).getCause();
+          }
+          Status status = Status.INTERNAL_SERVER_ERROR;
+          if(source instanceof NoSuchRecordException) {
+            status = Status.NOT_FOUND;
+          }
+          return Response.status(status).build();
+        } else  {
+          return Response.ok(Fruit.from(record.get("f").asNode())).build();
+        }
+      })
+      .thenCompose(response -> session.closeAsync().thenApply(signal -> response));
+  }
+
+  @POST
+  public CompletionStage<Response> create(Fruit fruit) {
+    AsyncSession session = driver.asyncSession();
+    return session
+      .writeTransactionAsync(tx ->
+        tx.runAsync("CREATE (f:Fruit {name: $name}) RETURN f", Values.parameters("name", fruit.name))
+      )
+      .thenCompose(fn -> fn.singleAsync())
+      .thenApply(record -> Fruit.from(record.get("f").asNode()))
+      .thenCompose(persistedFruid -> session.closeAsync().thenApply(signal -> persistedFruid))
+      .thenApply(persistedFruid -> Response
+        .created(URI.create("/fruits/" + persistedFruid.id))
+        .build()
+      );
   }
 
 }
