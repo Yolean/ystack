bf846d4803b392661d9b55d39df64f1139c0bc15
Staffan Olsson <staffan@repos.se>
2020-06-21 08:41:51 +0200

    Helps us detect y-cluster-assert-install issues

--- specs/monitoring.spec.js
+++ specs/monitoring.spec.js
@@ -12,6 +12,22 @@ describe("Monitoring", () => {
       expect(alerts).toBeInstanceOf(Array);
     });
 
+    it("Prometheus has the kubernetes-assert PodMonitor", async () => {
+      const config = await fetch('http://monitoring.ystack:9090/api/v1/status/config').then(res => res.json());
+      expect(config).toHaveProperty('data.yaml');
+      expect(config.data.yaml).toMatch(/job_name: monitoring\/kubernetes-assert\/0/);
+    });
+
+    it("Prometheus finds at lest one target (this one) for the PodMonitor", async () => {
+      const targets = await fetch('http://monitoring.ystack:9090/api/v1/targets?state=active').then(res => res.json());
+      expect(targets).toHaveProperty('data.activeTargets');
+      expect(targets.data.activeTargets).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({scrapePool: 'monitoring/kubernetes-assert/0'})
+        ])
+      );
+    });
+
   });
 
 });
